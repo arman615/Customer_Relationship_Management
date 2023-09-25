5ca8cd27d67925d9ebcfb11b8f3efb72fdd71bc4
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 21:37:23 +0400

    Create Customer.h

---
+++ Customer.h
@@ -0,0 +1,17 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+
+class Customer {
+private:
+    std::string name;
+    std::string contactInfo;
+
+public:
+    Customer(const std::string& name, const std::string& contactInfo);
+    const std::string& getName() const;
+    const std::string& getContactInfo() const;
+};
+
+#endif // CUSTOMER_H
