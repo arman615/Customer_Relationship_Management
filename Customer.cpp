c75beed338c466cc95f50008e22901d5321bf573
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 22:03:04 +0400

    Update Customer.cpp

--- Customer.cpp
+++ Customer.cpp
@@ -1,12 +1,12 @@
 #include "Customer.h"
 
-Customer::Customer(const std::string& name, const std::string& contactInfo)
+Customer::Customer(const std::string &name, const std::string &contactInfo)
     : name(name), contactInfo(contactInfo) {}
 
-const std::string& Customer::getName() const {
+const std::string &Customer::getName() const {
     return name;
 }
 
-const std::string& Customer::getContactInfo() const {
+const std::string &Customer::getContactInfo() const {
     return contactInfo;
 }
