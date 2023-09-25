853541e3bc60417f77db4b39b8ccf90c8e348c09
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 21:52:48 +0400

    Create Complaint.cpp

---
+++ Complaint.cpp
@@ -0,0 +1,16 @@
+#include "Complaint.h"
+
+Complaint::Complaint(const Sale& sale, const std::string& user, const std::string& description)
+    : sale(sale), user(user), description(description) {}
+
+const Sale& Complaint::getSale() const {
+    return sale;
+}
+
+const std::string& Complaint::getUser() const {
+    return user;
+}
+
+const std::string& Complaint::getDescription() const {
+    return description;
+}
