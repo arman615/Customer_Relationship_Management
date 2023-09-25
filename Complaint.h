952891a69e900e64b260d332afb5eadc4ba65af8
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 21:52:18 +0400

    Create Complaint.h

---
+++ Complaint.h
@@ -0,0 +1,20 @@
+#ifndef COMPLAINT_H
+#define COMPLAINT_H
+
+#include "Sale.h"
+#include <string>
+
+class Complaint {
+private:
+    Sale sale;
+    std::string user;
+    std::string description;
+
+public:
+    Complaint(const Sale& sale, const std::string& user, const std::string& description);
+    const Sale& getSale() const;
+    const std::string& getUser() const;
+    const std::string& getDescription() const;
+};
+
+#endif // COMPLAINT_H
