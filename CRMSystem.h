c69af2c373e159f71c732c5b8c6075199b7af690
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 21:55:10 +0400

    Update CRMSystem.h

--- CRMSystem.h
+++ CRMSystem.h
@@ -3,18 +3,27 @@
 
 #include "Customer.h"
 #include "Sale.h"
+#include "WarrantyService.h"
+#include "Complaint.h"
 #include <vector>
-#include <iostream>
+#include <string>
 
 class CRMSystem {
 private:
     std::vector<Customer> customers;
     std::vector<Sale> sales;
+    std::vector<WarrantyService> warrantyServices;
+    std::vector<Complaint> complaints;
+
+    void saveReportToFile(const Customer& customer, const std::string& report);
 
 public:
     void registerCustomer(const std::string& name, const std::string& contactInfo);
     void recordSale(const Sale& sale);
+    void recordWarrantyService(const WarrantyService& warrantyService);
+    void recordComplaint(const Complaint& complaint);
     void generateReport(const std::string& customerName);
+    void generateAndSaveReportToFile(const std::string& customerName);
 };
 
 #endif // CRMSYSTEM_H
