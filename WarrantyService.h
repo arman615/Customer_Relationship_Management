deb5223d0df4cb66d2f8b3d4824a90e4d43f4c64
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 22:04:05 +0400

    Update WarrantyService.h

--- WarrantyService.h
+++ WarrantyService.h
@@ -1,5 +1,5 @@
-#ifndef WARRANTYSERVICE_H
-#define WARRANTYSERVICE_H
+#ifndef WARRANTIESERVICE_H
+#define WARRANTIESERVICE_H
 
 #include "Sale.h"
 #include <string>
@@ -11,10 +11,10 @@ private:
     double serviceCost;
 
 public:
-    WarrantyService(const Sale& sale, const std::string& user, double serviceCost);
-    const Sale& getSale() const;
-    const std::string& getUser() const;
+    WarrantyService(const Sale &sale, const std::string &user, double serviceCost);
+    const Sale &getSale() const;
+    const std::string &getUser() const;
     double getServiceCost() const;
 };
 
-#endif // WARRANTYSERVICE_H
+#endif // WARRANTIESERVICE_H
