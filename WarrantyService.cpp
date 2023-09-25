f6eebcc77788ab15f4258ed7eb8ed607c29f5e88
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 22:04:18 +0400

    Update WarrantyService.cpp

--- WarrantyService.cpp
+++ WarrantyService.cpp
@@ -1,13 +1,13 @@
 #include "WarrantyService.h"
 
-WarrantyService::WarrantyService(const Sale& sale, const std::string& user, double serviceCost)
+WarrantyService::WarrantyService(const Sale &sale, const std::string &user, double serviceCost)
     : sale(sale), user(user), serviceCost(serviceCost) {}
 
-const Sale& WarrantyService::getSale() const {
+const Sale &WarrantyService::getSale() const {
     return sale;
 }
 
-const std::string& WarrantyService::getUser() const {
+const std::string &WarrantyService::getUser() const {
     return user;
 }
 
