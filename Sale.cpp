53ebdb14928a7e10226713fd9f56d0e734e43bda
arman615 <91202926+arman615@users.noreply.github.com>
2023-09-25 22:03:44 +0400

    Update Sale.cpp

--- Sale.cpp
+++ Sale.cpp
@@ -1,13 +1,13 @@
 #include "Sale.h"
 
-Sale::Sale(const std::string& date, const std::string& user, double amount, int warrantyPeriod)
+Sale::Sale(const std::string &date, const std::string &user, double amount, int warrantyPeriod)
     : date(date), user(user), amount(amount), warrantyPeriod(warrantyPeriod) {}
 
-const std::string& Sale::getDate() const {
+const std::string &Sale::getDate() const {
     return date;
 }
 
-const std::string& Sale::getUser() const {
+const std::string &Sale::getUser() const {
     return user;
 }
 
